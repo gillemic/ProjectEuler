98f80a4ab47563c662030747a39c1aada8a84e20
Michael Gillett <michaelgillett19@gmail.com>
2020-05-19 13:17:15 -0700

    refactored

---
+++ python/problem30.py
@@ -0,0 +1,30 @@
+import time
+
+#time at the start of program execution
+start = time.time()
+
+#Let the sum be 0 at the start
+solution = 0
+n = 5
+
+#for loop to loop till 5(10-1)^5
+def find_solution(n):
+    solution = 0
+    for i in range(2, n*(9**n)+1):
+        if sum([int(x)**n for x in str(i)]) == i:
+            solution += i
+            print(i)
+    return solution
+
+#printing the solution
+def main():
+    print(find_solution(5))
+
+if __name__ == '__main__':
+    main()
+
+#time at the end of execution
+end = time.time()
+
+#printing the total execution time
+print(end - start)
