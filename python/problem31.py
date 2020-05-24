00c7bce92f3a3910a5be8a4fd1801b1ff8d6947d
Michael Gillett <michaelgillett19@gmail.com>
2020-05-23 20:31:24 -0700

    problem 31 solution

--- python/problem31.py
+++ python/problem31.py
@@ -2,8 +2,18 @@ import time
 
 start = time.time()
 
+def find_combos(n):
+    coins = [1, 2, 5, 10, 20, 50, 100, 200]
+    ways = [1] + [0]*n
+
+    for coin in coins:
+        for i in range(coin, n+1):
+            ways[i] += ways[i-coin]
+
+    print("Ways to make change = {}".format(ways[n]))
+
 def main():
-    pass
+    find_combos(200)
 
 if __name__ == '__main__':
     main()
