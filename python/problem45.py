553d47419888e6a360311ac6c140f9576b5f42ba
Michael Gillett <michaelgillett19@gmail.com>
2020-07-26 14:03:33 -0700

    problem 45 + comments

--- python/problem45.py
+++ python/problem45.py
@@ -3,29 +3,27 @@ import math
 
 start = time.time()
 
-def is_triangle(n):
-  square_root = math.floor(math.sqrt(n*2))
-  if (square_root*(square_root+1) == n*2):
-    return True
-  return False
-
 def is_pentagonal(n):
+  #formula for finding pentagon nums
   if (1+(24*n+1)**0.5) % 6 == 0:
     return True
   return False
 
 def is_hexagonal(n):
+  #formula for finding hexagonal nums
   if (1+(8*n + 1)**0.5) % 4 == 0:
     return True
   return False
 
 def find_nums():
-  flag = True
-  i = 40756
+  #generate triangle nums starting at 286th term, 100,000 was arbitrary limit- could be anything
   triangle_nums = [i*(i+1)/2 for i in range(286, 100000)]
+
+  #iterate through triangle nums and check if they are also pentagonal and hexagonal
   for n in triangle_nums:
     if (is_pentagonal(n) and is_hexagonal(n)):
-      print("Next num: %d" % n)
+      index = triangle_nums.index(n)
+      print("Next num: %d at index %d" % (n, index+286))
       return
 
 def main():
